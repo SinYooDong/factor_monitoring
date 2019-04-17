cb06acbf222e59ec2389a0c3dcd19b133d4e2985
ud4mo <sud4mo@gmail.com>
2019-04-17 21:33:59 +0900

    tylescrip로 변경 및 저장.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import {Button} from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <Button color="danger">아하</Button>
+    );
+  }
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import simulator from "./simulator"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router>
+    <Route path="/" exact component={App} />
+    <Route path="/simulator" exact component={simulator} />
+</Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
